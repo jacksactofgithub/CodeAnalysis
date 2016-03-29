a43c203c86fcd7e4557be060c3631e57f866633c
Jack <2653909025@qq.com>
2016-03-29 20:17:31 +0800

    更改各种springmvc

--- src/pkg/QuestionSta.java
+++ src/pkg/QuestionSta.java
@@ -1,36 +1,27 @@
 package pkg;
 
-import java.io.IOException;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Servlet implementation class TestMatch
  */
-@WebServlet("/questionSta")
-public class QuestionSta extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+@Controller
+public class QuestionSta {
+	
     public QuestionSta() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		JSONObject runResultJson = null;//运行结果json
-    	//{stuid:,examNo考试号:,questionNo:,caseNum:测试用例数目,result:[{time:从考试开始算起,passNo:,failNo:},{time:,passNo:,failNo:},...]}//运行结果的json
-    	try {
+    @RequestMapping("/questionSta")
+    public String showStatistics(Model model,HttpServletRequest request){
+    	JSONObject runResultJson = null;
+		try {
     		String obj1="{'stuid':121250088,'examNo':1,'questionNo':'1','caseNum':6,'caseName':[triangle1,triangle2,triangle3,triangle4,triangle5,triangle6],'result':[{'time':1,'passNo':[1,2]},"
     				+ "{'time':3,'passNo':[1,2,3]},{'time':8,'passNo':[1,2,3,4]},{'time':11,'passNo':[1,2,3,4,5]},"
     				+ "{'time':14,'passNo':[1,2,3,4,5,6]}]}"; 
@@ -39,14 +30,11 @@ public class QuestionSta extends HttpServlet {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-    	
     	//
-    	request.setAttribute("runResultJson", runResultJson);//测试用例通过情况的json
+    	request.setAttribute("runResultJson", runResultJson); 
     	
-    	//代码统计数据jsonArray格式[{"timestamp": double, "source":string , "lineCount":int , "noteCount":int , "methodCount": int , "varyCount": int , "maxCyclomaticCpl": int},
-    	//{"timestamp": double, "source":string , "lineCount":int , "noteCount":int , "methodCount": int , "varyCount": int , "maxCyclomaticCpl": int}]
-    	//JSONObject codeSta =null;//代码统计json;调用刘睿模块
-    		String obj3="[{'timestamp': 1, 'source':'q1' , 'lineCount':5 , 'noteCount':0 , 'methodCount': 1 , 'varyCount': 2 , 'maxCyclomaticCpl': 1},"
+    		@SuppressWarnings("unused")
+			String obj3="[{'timestamp': 1, 'source':'q1' , 'lineCount':5 , 'noteCount':0 , 'methodCount': 1 , 'varyCount': 2 , 'maxCyclomaticCpl': 1},"
     				+ "{{'timestamp': 2, 'source':'q1' , 'lineCount':6 , 'noteCount':0 , 'methodCount': 1 , 'varyCount': 2 , 'maxCyclomaticCpl': 1},"
     				+ "{'timestamp': 3, 'source':'q1' , 'lineCount':6 , 'noteCount':0 , 'methodCount': 1 , 'varyCount': 2 , 'maxCyclomaticCpl': 1},"
     				+ "{'timestamp': 4, 'source':'q1' , 'lineCount':6 , 'noteCount':0 , 'methodCount': 1 , 'varyCount': 2 , 'maxCyclomaticCpl': 1},"
@@ -55,20 +43,17 @@ public class QuestionSta extends HttpServlet {
     				+ "{'timestamp': 7, 'source':'q1' , 'lineCount':6 , 'noteCount':0 , 'methodCount': 1 , 'varyCount': 2 , 'maxCyclomaticCpl': 1},"
     				+ "{'timestamp': 8, 'source':'q1' , 'lineCount':6 , 'noteCount':0 , 'methodCount': 1 , 'varyCount': 2 , 'maxCyclomaticCpl': 1},"
     				+ "{'timestamp': 9, 'source':'q1' , 'lineCount':6 , 'noteCount':0 , 'methodCount': 1 , 'varyCount': 2 , 'maxCyclomaticCpl': 1}]";
-    		//reverse(obj3);
-    		//解析成另一种格式{timestamp:[],source:,lineCount:[],noteCount[],methodCount:[],varCount:[],maxCy:[]}
     	
     	String obj4 = "{'source':'q1','timestamp':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],'lineCount':[0,2,7,8,15,16,20,25,27,28,35,36,30,35,37],"
     			+ "'noteCount':[0,1,3,3,5,6,6,8,10,3,5,6,6,8,10],'methodCount':[0,1,1,1,1,2,2,2,2,1,1,2,2,3,3],"
     			+ "'varCount':[0,1,3,4,5,5,5,6,6,4,5,5,5,6,6],'maxCy':[1,1,1,2,2,2,3,3,3,2,2,2,3,3,3]}";
-    	request.setAttribute("staJsonstr", obj4);//测试用例通过情况的json
+    	request.setAttribute("staJsonstr", obj4); 
     	try {
 			request.setAttribute("staJson", new JSONObject(obj4));
 		} catch (JSONException e) {
 			e.printStackTrace();
-		}//测试用例通过情况的json
-		ServletContext context = request.getServletContext();
-		context.getRequestDispatcher("/view/questionSta.jsp").forward(request, response);
-	}
-
+		} 
+    	return "view/questionSta";
+    }
+    
 }
--- src/pkg/StuAnalysis.java
+++ src/pkg/StuAnalysis.java
@@ -1,35 +1,24 @@
 package pkg;
 
-import java.io.IOException;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Servlet implementation class StudentSta
  */
-@WebServlet("/StuAnalysis")
-public class StuAnalysis extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+@Controller
+public class StuAnalysis {
+	
     public StuAnalysis() {
         super();
     }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ServletContext context = request.getServletContext();
-		context.getRequestDispatcher("/view/stuanalysis.jsp").forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+    
+    @RequestMapping("/stuAnalysis")
+    public String studentAnalysis(Model model,HttpServletRequest request){
+		return "view/stuanalysis";
 	}
 
 }
