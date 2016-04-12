26f87a4c89ae004b3ad7cbf65f55b50d21a8a210
刘睿 <m15861816378@163.com>
2016-04-12 23:01:29 +0800

    update problemService

--- src/pkg/service/impl/ProblemServiceImpl.java
+++ src/pkg/service/impl/ProblemServiceImpl.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.json.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import pkg.service.ExamService;
 import pkg.service.ProblemService;
 import util.HttpHandler;
 import util.URLNameFormat;
@@ -14,12 +16,20 @@ import util.URLNameFormat;
 @Service
 public class ProblemServiceImpl implements ProblemService{
 
+	@Autowired
+	private ExamService examService;
+	
 	private HttpHandler http = new HttpHandler();
 	
 	@Override
 	public JSONArray getExamProblems(int stu_id, int exam_id) {
 		// TODO Auto-generated method stub
 		Map<String , String> params = new HashMap<String , String>();
+		
+		int classMemberId = examService.getClassMemberId(stu_id, exam_id);
+		params.put(URLNameFormat.CLASS_MEMBER_ID, classMemberId+"");
+		params.put(URLNameFormat.EXAM_ID, exam_id+"");
+		
 		try {
 			String problems = http.postHttpInvocation(URLNameFormat.GET_STUDENT_EXAM_PROBLEMS, params);
 			JSONArray array = new JSONArray(problems);
