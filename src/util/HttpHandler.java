f99f3162c0423b51747b85f0e16966b9c1ed75c9
刘睿 <m15861816378@163.com>
2016-04-14 14:08:00 +0800

    cmt

--- src/util/HttpHandler.java
+++ src/util/HttpHandler.java
@@ -19,7 +19,7 @@ import java.util.Map.Entry;
  */
 public class HttpHandler {
 
-	private static final String INVOCATION_URL = "http://127.0.0.1:9000/service/";
+	public static String INVOCATION_URL;
 	
 	public String postHttpInvocation(String interfaceName , Map<String , String> params) throws Exception{ 
 		
