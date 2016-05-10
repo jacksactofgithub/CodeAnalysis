81cc86b01c4e6c354ed94e7afe4f745e11cd3045
Jack <2653909025@qq.com>
2016-05-10 20:51:59 +0800

    1

--- src/pkg/controller/student/StudentAnalysis.java
+++ src/pkg/controller/student/StudentAnalysis.java
@@ -7,7 +7,6 @@ import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import pkg.service.CodeService;
 import pkg.service.ExamService;
@@ -29,14 +28,19 @@ public class StudentAnalysis {
     /**
      * 学生点击考试分析页面;显示自己参加过的考试
      * @param request
-     * @return
+     * @return String view
      */
     
     @RequestMapping("/stuAnalysis")
-    public String studentAnalysis(HttpServletRequest request,HttpSession session,@RequestParam("stu_id")int stu_id,
-    		@RequestParam("stu_name")String stu_name,@RequestParam("uni_name")String uni_name){
+    public String studentAnalysis(HttpServletRequest request,HttpSession session){
     	//首先看request中有没有参数 如果有设置session否则去session中找
-    	if(stu_id!=0&&stu_name!=null){
+    	int stu_id=-1;
+    	if(request.getParameter("stu_id")!=null){
+    		stu_id = Integer.parseInt(request.getParameter("stu_id"));
+    	}
+    	String stu_name = request.getParameter("stu_name");
+    	String uni_name = request.getParameter("uni_name");
+    	if(stu_id!=-1&&stu_name!=null){
         	//在第一次进的时候如果request中带了参数则将session中参数设置为登录信息;否则session中信息不变(如果是本网站的请求)
         	session.setAttribute("stu_name", stu_name);
         	session.setAttribute("uni_name", uni_name);
